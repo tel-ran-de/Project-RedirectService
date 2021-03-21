7dbfae00ea574fb1681ad38780d52b3c58dc9c04
Ievgenij_Pavlenko <evgenij.pavlenko@gmail.com>
2021-03-21 01:10:23 +0100

    Redirect Service: API definition for URL creation - add Controller

--- src/main/java/de/telran/project_redirect_service/controller/ShortLongUrlController.java
+++ src/main/java/de/telran/project_redirect_service/controller/ShortLongUrlController.java
@@ -1,13 +1,23 @@
 package de.telran.project_redirect_service.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class ShortLongUrlController {
 
+    @Autowired
+    private UrlService urlService;
+
     @GetMapping("short_server/{urlCode}")
     public String redirectFromShortToLongUrl(@PathVariable String urlCode) {
         String longUrl = urlCode.toUpperCase();
         return "redirect:/" + longUrl;
+    }
+
+    @PostMapping("server/urls")
+    public ShortUrl createShortUrlFromLong(@RequestBody LongUrl longUrl){
+        ShortUrl shortUrl = urlService.createShortUrl(longUrl);
+        return shortUrl;
     }
 }
