114788bec234ac651d799f0fa48c34347fa64850
Ievgenij_Pavlenko <evgenij.pavlenko@gmail.com>
2021-03-25 19:13:05 +0100

    change on RedirectView

--- src/main/java/de/telran/project_redirect_service/controller/RedirectController.java
+++ src/main/java/de/telran/project_redirect_service/controller/RedirectController.java
@@ -3,14 +3,10 @@ package de.telran.project_redirect_service.controller;
 import de.telran.project_redirect_service.service.RedirectService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.view.RedirectView;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 import java.util.Optional;
 
 @RestController
@@ -19,11 +15,14 @@ public class RedirectController {
     RedirectService redirectService;
 
     @GetMapping("/{urlCode}")
-    public String redirect(@RequestParam String shortUrlCode) {
+    public RedirectView redirect(@RequestParam String shortUrlCode) {
         Optional optional = redirectService.getRedirectUrl(shortUrlCode);
+        RedirectView redirectView = new RedirectView();
         if (optional.isPresent()) {
-            return "redirect:" + optional.toString();
+            redirectView = new RedirectView(optional.toString());
+            return redirectView;
+//            return "redirect:" + optional.toString();
         }
-        return Optional.empty().toString();
+        return null;
     }
 }
