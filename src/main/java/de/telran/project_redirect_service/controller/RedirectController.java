9cec30f0427051329b6a00751ea60ed7a74aa648
Ievgenij_Pavlenko <evgenij.pavlenko@gmail.com>
2021-03-31 14:16:31 +0200

    change on RedirectView

--- src/main/java/de/telran/project_redirect_service/controller/RedirectController.java
+++ src/main/java/de/telran/project_redirect_service/controller/RedirectController.java
@@ -2,7 +2,9 @@ package de.telran.project_redirect_service.controller;
 
 import de.telran.project_redirect_service.service.RedirectService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.view.RedirectView;
@@ -11,18 +13,14 @@ import java.util.Optional;
 
 @RestController
 public class RedirectController {
-    @Autowired
-    RedirectService redirectService;
 
-    @GetMapping("/{urlCode}")
-    public RedirectView redirect(@RequestParam String shortUrlCode) {
-        Optional optional = redirectService.getRedirectUrl(shortUrlCode);
+    @Autowired
+    private RedirectService redirectService;
+
+    @GetMapping("/{shortUrl}")
+    public ResponseEntity<String> redirect(@PathVariable String shortUrl) {
+        Optional<String> optional = redirectService.getRedirectUrl(shortUrl);
         RedirectView redirectView = new RedirectView();
-        if (optional.isPresent()) {
-            redirectView = new RedirectView(optional.toString());
-            return redirectView;
-//            return "redirect:" + optional.toString();
-        }
-        return null;
+        return optional.map(c->ResponseEntity.status(302).body(c)).orElse(ResponseEntity.notFound().build());
     }
 }
--- src/main/java/de/telran/project_redirect_service/controller/UrlShortenerController.java
+++ src/main/java/de/telran/project_redirect_service/controller/UrlShortenerController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class UrlShortenerController {
 
     @Autowired
-    UrlShortenerService service;
+    private UrlShortenerService service;
 
     @PostMapping("/urls")
     public ShortUrlDto generateShortUrl(LongUrlDto longUrlDto) {
