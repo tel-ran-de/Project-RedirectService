3bd2ad910c97bc8992de061c3d5a84d860b1fd52
Stingrayoliv <64040033+Stingrayoliv@users.noreply.github.com>
2021-04-13 16:15:49 +0200

    test Successful scenario redirect" with using with REST-assured / Integration Test

---
+++ src/test/java/de/telran/projectredirectservice/units/ApiUrls.java
@@ -0,0 +1,14 @@
+package de.telran.projectredirectservice.units;
+
+public class ApiUrls {
+
+    //    ------------  Commons  ---------------
+    public static String baseUrl = "http://localhost:8090";
+
+
+//  -----------------  Endpoints  -------------------------
+
+    public static String urlShortenerAPIURL = baseUrl + "/api/urls";
+    public static String redirectAPIUrl = baseUrl + "/short_server/";
+
+}
